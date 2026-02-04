e6b8eac968548dc590ce9a6bf6b9229e35b3ffc2
IsaacOrlando-web <isaacserviciosweb@gmail.com>
2026-02-04 10:45:56 -0300

    getSingle movie created

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongodb = require('../db/database');
+const ObjectId = require('mongodb').ObjectId;
+
 
 const getAllMovies = async (req, res) => {
     try{
@@ -15,4 +17,16 @@ const getAllMovies = async (req, res) => {
     }
 }
 
-module.exports = { getAllMovies };
+const getSingleMovie = async (req, res) => {
+    try {
+        const movieId = new ObjectId(req.params.id);
+        await mongodb.initDB();
+        const result = await mongodb.getDatabase().collection('movies').findOne({ _id: movieId });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: "Error retrieving movie", error: error.message });
+    }
+}
+
+module.exports = { getAllMovies, getSingleMovie };
--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const moviesController = require('../controllers/moviesController');
 
 router.get('/movies', moviesController.getAllMovies);
+router.get('/movies/:id', moviesController.getSingleMovie);
 
 module.exports = router;
