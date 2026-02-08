4344f51f8fa81ac623fcef6e2cce7383e76ede8a
carsonlandvatter <landvattercarson@gmail.com>
2026-02-08 15:58:27 -0700

    Fixed put routes and updated added swagger

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(express.json());
 
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
